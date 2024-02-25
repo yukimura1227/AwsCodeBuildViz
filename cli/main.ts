536991913f482f3f6211959c216fca356ffd2bad
yukimura1227 <takamura1227@gmail.com>
2024-02-25 13:20:26 +0900

    feat: store buildIds in DenoKV

--- cli/main.ts
+++ cli/main.ts
@@ -3,6 +3,24 @@ import { ListBuilds } from "./libs/ListBuilds.ts";
 const awsProfileName:string = Deno.args[0];
 const codeBuildProjectName:string = Deno.args[1];
 
+const kv = await Deno.openKv();
+
 const buildIds = await ListBuilds(awsProfileName, codeBuildProjectName);
 
 console.log(buildIds);
+
+for(const buildId of buildIds) {
+  await kv.set(
+    [codeBuildProjectName, buildId],
+    {
+      codeBuildProjectName,
+      buildId,
+    }
+  );
+}
+
+const entries = kv.list({ prefix: [codeBuildProjectName]});
+for await (const entry of entries) {
+  console.log(entry.key);
+  console.log(entry.value);
+}
