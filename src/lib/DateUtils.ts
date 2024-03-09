18217944d4b52d610d2f57aeb86fdd16a98b8935
yukimura1227 <takamura1227@gmail.com>
2024-03-09 12:34:19 +0900

    refact: extract dateToyyyyMMddWithHyphen()

--- src/lib/DateUtils.ts
+++ src/lib/DateUtils.ts
@@ -1,4 +1,8 @@
 // yyyy-MM-dd形式の日付を取得
 export const convertDateToDayString = (date: Date):string => {
-  return new Date(date).toLocaleDateString("ja-JP", {year: "numeric", month: "2-digit", day: "2-digit"}).slice(0,10);
+  return dateToyyyyMMddWithHyphen(date).slice(0,10);
+}
+
+const dateToyyyyMMddWithHyphen = (date: Date):string => {
+  return new Date(date).toLocaleDateString("ja-JP", {year: "numeric", month: "2-digit", day: "2-digit"});
 }
