4a3f2a5d3cd8ac255ea57f2f3d7ef1ce35f13b47
yukimura1227 <takamura1227@gmail.com>
2025-05-10 09:34:11 +0900

    chore: deno run lint:fix to cli

--- cli/libs/BatchGetBuilds.ts
+++ cli/libs/BatchGetBuilds.ts
@@ -1,6 +1,14 @@
-import { CodeBuildClient, BatchGetBuildsCommand, type BatchGetBuildsCommandInput, type BatchGetBuildsCommandOutput } from "npm:@aws-sdk/client-codebuild";
+import {
+  BatchGetBuildsCommand,
+  type BatchGetBuildsCommandInput,
+  type BatchGetBuildsCommandOutput,
+  CodeBuildClient,
+} from 'npm:@aws-sdk/client-codebuild';
 
-const createClient = (credentials:unknown, region:string):CodeBuildClient => {
+const createClient = (
+  credentials: unknown,
+  region: string
+): CodeBuildClient => {
   const client = new CodeBuildClient({
     region: region,
     credentials: credentials,
@@ -8,20 +16,27 @@ const createClient = (credentials:unknown, region:string):CodeBuildClient => {
   return client;
 };
 
-const getButchBuild = async (client:CodeBuildClient, buildId:string):BatchGetBuildsCommandOutput => {
-  const input:BatchGetBuildsCommandInput = {
+const getButchBuild = async (
+  client: CodeBuildClient,
+  buildId: string
+): BatchGetBuildsCommandOutput => {
+  const input: BatchGetBuildsCommandInput = {
     ids: [buildId],
   };
   const command = new BatchGetBuildsCommand(input);
 
-  const response:BatchGetBuildsCommandOutput = await client.send(command);
+  const response: BatchGetBuildsCommandOutput = await client.send(command);
 
   console.log(response);
 
   return response;
-}
+};
 
-export const BatchGetBuilds = async (credentials:unknown, buildId:string, region:string) => {
+export const BatchGetBuilds = async (
+  credentials: unknown,
+  buildId: string,
+  region: string
+) => {
   const client = await createClient(credentials, region);
 
   return await getButchBuild(client, buildId);
--- cli/libs/ListBuilds.ts
+++ cli/libs/ListBuilds.ts
@@ -1,6 +1,11 @@
-import { CodeBuildClient, ListBuildsForProjectCommand, ListBuildsForProjectCommandInput, ListBuildsForProjectCommandOutput } from "npm:@aws-sdk/client-codebuild";
+import {
+  CodeBuildClient,
+  ListBuildsForProjectCommand,
+  type ListBuildsForProjectCommandInput,
+  type ListBuildsForProjectCommandOutput,
+} from 'npm:@aws-sdk/client-codebuild';
 
-const createClient = (credentials:unknown,region:string) => {
+const createClient = (credentials: unknown, region: string) => {
   const client = new CodeBuildClient({
     region: region,
     credentials: credentials,
@@ -8,33 +13,49 @@ const createClient = (credentials:unknown,region:string) => {
   return client;
 };
 
-const listBuildsOnce = async (client:unknown, buildIdsResult:string[], codeBuildProjectName: string, nextToken?: string) => {
-  const input:ListBuildsForProjectCommandInput = {
+const listBuildsOnce = async (
+  client: unknown,
+  buildIdsResult: string[],
+  codeBuildProjectName: string,
+  nextToken?: string
+) => {
+  const input: ListBuildsForProjectCommandInput = {
     projectName: codeBuildProjectName,
     nextToken: nextToken,
   };
   const command = new ListBuildsForProjectCommand(input);
 
-  const response:ListBuildsForProjectCommandOutput = await client.send(command);
+  const response: ListBuildsForProjectCommandOutput =
+    await client.send(command);
 
-  nextToken = response.nextToken;
+  const nextNextToken = response.nextToken;
 
-  console.log({nextToken});
-  console.log({length: response.ids.length});
+  console.log({ nextToken });
+  console.log({ length: response.ids.length });
 
-  for(const buildId of response.ids) {
+  for (const buildId of response.ids) {
     buildIdsResult.push(buildId);
   }
 
   console.log(buildIdsResult.length);
-  if(nextToken) await listBuildsOnce(client, buildIdsResult, codeBuildProjectName, nextToken);
-}
+  if (nextNextToken)
+    await listBuildsOnce(
+      client,
+      buildIdsResult,
+      codeBuildProjectName,
+      nextNextToken
+    );
+};
 
-export const ListBuilds = async (credentials:unknown, codeBuildProjectName: string, region: string):Promise<string[]> => {
+export const ListBuilds = async (
+  credentials: unknown,
+  codeBuildProjectName: string,
+  region: string
+): Promise<string[]> => {
   const client = await createClient(credentials, region);
 
-  console.log('実行開始')
-  const buildIdsResult:string[] = [];
+  console.log('実行開始');
+  const buildIdsResult: string[] = [];
   await listBuildsOnce(client, buildIdsResult, codeBuildProjectName);
   return buildIdsResult;
 };
